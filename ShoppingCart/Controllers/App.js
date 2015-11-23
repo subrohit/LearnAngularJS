4f9e5aadf6cbceec54aaacf7fe1a8a8da6dbc669
RamiV <rami.ramilu@gmail.com>
2015-11-23 15:20:21 +0530

    1. Added Order Shippment View 2. Added Form Validation

--- ShoppingCart/Controllers/App.js
+++ ShoppingCart/Controllers/App.js
@@ -10,6 +10,15 @@
         caseInsensitiveMatch: true
     });
 
+    $routeProvider.when("/complete", {
+        templateUrl: "/../Views/Partials/ThankYou.html",
+        caseInsensitiveMatch: true
+    });
+    $routeProvider.when("/placeorder", {
+        templateUrl: "../Views/Partials/PlaceOrder.html",
+        caseInsensitiveMatch: true
+    });
+
     $routeProvider.otherwise({
         templateUrl: "../Views/Partials/ProductList.html",
         caseInsensitiveMatch: true
