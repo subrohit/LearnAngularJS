f768eb15a2c96f0f6089d2eb937e2f9e672acca6
RamiV <rami.ramilu@gmail.com>
2015-11-24 11:23:25 +0530

    Web API GetAllProducts End Point

---
+++ ShoppingCart.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoppingCart.WebApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        [Route("api/GetAllProducts")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product { Category = "Watersports", Description = "A boat for one person", Name = "Kayak", Price = 275, Id = "05af70919155f8fc"},
+                new Product { Category = "Watersports", Description = "Protective and fashionable", Name = "Lifejacket", Price = 48.95m, Id = "3d31d81b218c98ef"},
+                new Product { Category = "Soccer", Description = "FIFA-approved size and weight", Name = "Soccer Ball", Price = 19.5m, Id = "437615faf1d38815"},
+                new Product { Category = "Soccer", Description = "Give your playing field a professional touch", Name = "Corner Flags", Price = 34.95m, Id = "93c9cc08ac2f28d4"},
+                new Product { Category = "Soccer", Description = "Flat-packed 35,000-seat stadium", Name = "Stadium", Price = 79500, Id = "ad4e64b38baa088f"},
+                new Product { Category = "Chess", Description = "Improve your brain efficiency by 75%", Name = "Thinking Cap", Price = 16, Id = "b9e8e55c1ecc0b63"},
+                new Product { Category = "Chess", Description = "Secretly give your opponent a disadvantage", Name = "Unsteady Chair", Price = 29.95m, Id = "32c2355f9a617bbd"},
+                new Product { Category = "Chess", Description = "A fun game for the family", Name = "Human Chess Board", Price = 75, Id = "5241512218f73a26"},
+                new Product { Category = "Chess", Description = "Gold-plated, diamond-studded King", Name = "Bling-Bling King", Price = 1200, Id = "59166228d70f8858"}
+            };
+        }
+    }
+
+    public class Product
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Id { get; set; }
+    }
+}
--- ShoppingCart.WebApi/Controllers/ValuesController.cs
+++ ShoppingCart.WebApi/Controllers/ValuesController.cs
@@ -7,7 +7,6 @@ using System.Web.Http;
 
 namespace ShoppingCart.WebApi.Controllers
 {
-    [Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
