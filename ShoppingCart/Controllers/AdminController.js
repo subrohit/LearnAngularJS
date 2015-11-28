293d378a5342f70d0c5adb948e0cc0bb4cb82e5a
RamiV <rami.ramilu@gmail.com>
2015-11-28 12:05:53 +0530

    Admin Module - Half Completed

---
+++ ShoppingCart/Controllers/AdminController.js
@@ -0,0 +1,17 @@
+﻿angular.module("AdminModule")
+.controller("AdminController",function ($scope) {
+    var menuItems = ["Products", "Orders"];
+    $scope.selectedMenuItem = 1;
+
+    $scope.getMenuItems = function () {
+        return menuItems;
+    }
+    $scope.setSelectedMenuItem = function (menuItemIndex) {
+        $scope.selectedMenuItem = menuItemIndex;
+    }
+
+    $scope.getScreen = function () {
+        return $scope.selectedMenuItem == 0 ? "../Views/Partials/AdminProducts.html" : "../Views/Partials/AdminOrders.html";
+    };
+
+})
---
+++ ShoppingCart/Controllers/AdminModule.js
@@ -0,0 +1,11 @@
+﻿angular.module("AdminModule", ["ngRoute", "ngAnimate"])
+.config(function ($routeProvider) {
+    $routeProvider.when("/main", {
+        templateUrl: "../Views/Admin.html",
+        caseInsensitiveMatch: true
+    });
+    $routeProvider.otherwise({
+        templateUrl: "../Views/Admin.html",
+        caseInsensitiveMatch: true
+    });
+})
